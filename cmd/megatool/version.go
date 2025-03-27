3384489b999214d9f9f2c088503ca16c42951658
Simon Collins <874670+simoncollins@users.noreply.github.com>
2025-03-27 21:03:11 +1000

    chore: bump version to v1.0.0-alpha.4

--- cmd/megatool/version.go
+++ cmd/megatool/version.go
@@ -1,4 +1,4 @@
 package main
 
 // Version is the current version of megatool
-const Version = "1.0.0-alpha.3" // Initial version - will be updated by the versioning command
+const Version = "v1.0.0-alpha.4" // Initial version - will be updated by the versioning command
